8ecd491fc5e2f11ea9755e24300784e37675f24f
Derek <derek@stepfarm.io>
2022-05-04 17:47:23 +0200

    set treasury for fees

--- code.js
+++ code.js
@@ -1,14 +1,20 @@
 const ONE_MINUTE = 60
 const ZEROES = '000000000000000000'
-const ADDRESS_ROUTER = '0x9a0a6763589c8bd20d8a83262af491e49f4e3efe'
+const ADDRESS_FACTORY = '0xF8E15161e9e6d59702B3fD9122516a8CB838b96F'
+const ADDRESS_ROUTER = '0xEF1B08D473913829a0a6ed32B4c37724bC31f096'
 const TOKENS = {
 	USDC: '0x64544969ed7ebf5f083679233325356ebe738930',
 	USDT: '0x337610d27c682e347c9cd60bd4b3b107c9d34ddd',
-	TEST: '0x84c6becf034ca616bef95e669fc65aa6dc3b1e53'
+	// TEST: '0x84c6becf034ca616bef95e669fc65aa6dc3b1e53'
 }
 const SLIPPAGE_TOLERANCE = 0.005
 let dweb3 = new Web3('https://data-seed-prebsc-1-s1.binance.org:8545/')
 dweb3.eth.getChainId().then(r => console.log('chain id: '+r))
+
+let ABI_FACTORY = null
+$.getJSON("abi/PancakeFactory.json", function(json) {
+	ABI_FACTORY = json.abi
+})
 
 let ABI_ROUTER = null
 $.getJSON("abi/PancakeRouter.json", function(json) {
@@ -20,7 +26,19 @@ $.getJSON("abi/PancakeERC20.json", function(json) {
 	ABI_ERC20 = json.abi
 })
 
-function onAddLiquidity () {
+function setFeeTo () {
+	let treasury = prompt("Please enter treasury address", "0x0000000000000000000000000000000000000000")
+	let web3 = new Web3(provider)
+	let contract = new web3.eth.Contract(ABI_FACTORY, ADDRESS_FACTORY)
+	contract
+		.methods
+		.setFeeTo(treasury)
+		.send({
+			from: selectedAccount
+		})
+}
+
+function addLiquidity () {
 	let web3 = new Web3(provider)
 	let contract = new web3.eth.Contract(ABI_ROUTER, ADDRESS_ROUTER)
 	let deadline = Math.round(new Date().getTime()/1000) + 3 * ONE_MINUTE
@@ -32,7 +50,7 @@ function onAddLiquidity () {
 		})
 }
 
-function onRemoveLiquidity () {
+function removeLiquidity () {
 	let web3 = new Web3(provider)
 	let contract = new web3.eth.Contract(ABI_ROUTER, ADDRESS_ROUTER)
 	let deadline = Math.round(new Date().getTime()/1000) + 3 * ONE_MINUTE
@@ -44,7 +62,7 @@ function onRemoveLiquidity () {
 		})
 }
 
-function onApproveToken () {
+function approveToken () {
 	let token = prompt("Please enter token", "USDC")
 	let tokenAddress = TOKENS[token]
 	if (!tokenAddress) {
@@ -63,7 +81,7 @@ function onApproveToken () {
 		})
 }
 
-function onGetQuote () {
+function getQuote () {
 	let tokenA = $('#tokenA-select').val()
 	let tokenAAddress = TOKENS[tokenA]
 	let tokenAAmount = BigInt($('#amountTokenA').val() * 10**18)
@@ -88,7 +106,7 @@ function onGetQuote () {
 		})
 }
 
-function onSwap () {
+function swap () {
 	let tokenA = $('#tokenA-select').val()
 	let tokenAAddress = TOKENS[tokenA]
 	let tokenAAmount = BigInt($('#amountTokenA').val() * 10**18)
@@ -121,7 +139,7 @@ function onSwap () {
 		})
 }
 
-function onGetBalance() {
+function getBalance() {
 	let token = prompt("Please enter token", "USDC")
 	let tokenAddress = TOKENS[token]
 	if (!tokenAddress) {
@@ -141,10 +159,11 @@ function onGetBalance() {
 
 window.addEventListener('load', async () => {
   init()
-	document.querySelector("#btn-getBalance").addEventListener("click", onGetBalance)
-  document.querySelector("#btn-addLiquidity").addEventListener("click", onAddLiquidity)
-	document.querySelector("#btn-removeLiquidity").addEventListener("click", onRemoveLiquidity)
-	document.querySelector("#btn-approveToken").addEventListener("click", onApproveToken)
-	document.querySelector("#btn-getQuote").addEventListener("click", onGetQuote)
-	document.querySelector("#btn-swap").addEventListener("click", onSwap)
+	document.querySelector("#btn-getBalance").addEventListener("click", getBalance)
+  document.querySelector("#btn-addLiquidity").addEventListener("click", addLiquidity)
+	document.querySelector("#btn-removeLiquidity").addEventListener("click", removeLiquidity)
+	document.querySelector("#btn-approveToken").addEventListener("click", approveToken)
+	document.querySelector("#btn-getQuote").addEventListener("click", getQuote)
+	document.querySelector("#btn-swap").addEventListener("click", swap)
+	document.querySelector("#btn-setTreasury").addEventListener("click", setFeeTo)
 })
