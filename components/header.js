c9db88e226785745e83262031907b92f63247506
uxlee <lee@stepfarm.io>
2022-05-16 09:00:24 -0500

    added header swap router link

--- components/header.js
+++ components/header.js
@@ -22,7 +22,7 @@ class Header extends HTMLElement {
           <!-- Collect the nav links, forms, and other content for toggling -->
           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-collapse-vertical-aligned">
-              <li><a href="#">FEATURES</a></li>
+              <li><a href="/swap">SWAP</a></li>
               <li><a href="#">PRICING</a></li>
               <li><a href="#">DOWNLOAD</a></li>
               <li class="dropdown">
