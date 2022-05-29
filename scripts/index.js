e029c8c8dd18eb4224cda737901e8e2b30f0937d
uxlee <lee@stepfarm.io>
2022-05-29 12:26:49 -0500

    added viewport script for dynamic content injection based on viewport width. Mobile view adjustments

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,16 @@
 // design related JS logic
+const PLACEHOLDER_1 = "Sweet and Tone is a seven-day bodyweight training program designed to boost your strength and endurance over the course of a week.";
+const PLACEHOLDER_2 = "Sweet and Tone is a seven-day bodyweight training program designed to boost your strength and";
+const PLACEHOLDER_3 = "Lorem Ipsum Dolor Sit Amet";
+
+window.addEventListener('resize', function () { // conditional rendering based on viewport width
+  if (viewportWidth <= 640) {
+    $('.span').text(PLACEHOLDER_3);
+  } else {
+    $('.span').text(PLACEHOLDER_1);
+  }
+}, false);
+
 window.addEventListener('load', function () {
   const counters = 4;
   for (let i = 1; i <= counters; i ++) {
