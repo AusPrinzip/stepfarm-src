127ffa71e1a2ecd1a4742ddfc237dace1c6a9681
uxlee <lee@stepfarm.io>
2022-05-29 14:12:37 -0500

    Further mobile port design development including conditional rendering of svgs and dedicated css

--- scripts/index.js
+++ scripts/index.js
@@ -4,14 +4,37 @@ const PLACEHOLDER_2 = "Sweet and Tone is a seven-day bodyweight training program
 const PLACEHOLDER_3 = "Lorem Ipsum Dolor Sit Amet";
 
 window.addEventListener('resize', function () { // conditional rendering based on viewport width
-  if (viewportWidth <= 640) {
+  if (viewportWidth <= 375) {
     $('.span').text(PLACEHOLDER_3);
+    $('.shoe').attr("src", "/images/shoe-mobile.svg");
+    $('.hero').attr("src", "/images/hero-mobile.svg");
+    $('.stat').hide()
+    $('.stats-rows').hide()
   } else {
     $('.span').text(PLACEHOLDER_1);
+    $('.shoe').attr("src", "/images/shoe.svg");
+    $('.hero').attr("src", "/images/hero.svg");
+    $('.stat').show()
+    $('.stats-rows').show()
   }
 }, false);
 
 window.addEventListener('load', function () {
+
+  if (viewportWidth <= 375) {
+    $('.span').text(PLACEHOLDER_3);
+    $('.shoe').attr("src", "/images/shoe-mobile.svg");
+    $('.hero').attr("src", "/images/hero-mobile.svg");
+    $('.stat').hide()
+    $('.stats-rows').hide()
+  } else {
+    $('.span').text(PLACEHOLDER_1);
+    $('.shoe').attr("src", "/images/shoe.svg");
+    $('.hero').attr("src", "/images/hero.svg");
+    $('.stat').show()
+    $('.stats-rows').show()
+  }
+
   const counters = 4;
   for (let i = 1; i <= counters; i ++) {
     const el = document.querySelector(`.counter${i}`);
