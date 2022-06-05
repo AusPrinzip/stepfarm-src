0d402c16ab09749d21da872597cc5a2c2517601d
uxlee <lee@stepfarm.io>
2022-06-05 10:00:35 -0500

    Reworked footer (now proper size on mobile). Added css-only spinner library

--- components/footer.js
+++ components/footer.js
@@ -5,24 +5,27 @@ class Div extends HTMLElement {
 
   connectedCallback () {
     this.innerHTML = `
-      <div id="footer">
-          <div class="twitter-container">
-            <a href="https://twitter.com/StepFarm" target="_blank"><img class="" src="images/icons/twitter.svg"></img></a>
-          </div>
-          <div class="discord-container">
-            <a target="https://twitter.com/StepFarm" href="https://discord.gg/gC72KwH4Gp" target="_blank"><img class="" src="images/icons/discord.svg"></img></a>
-          </div>
-          <div class="telegram-container">
-            <a href="https://t.me/+GugtFxYudhtiYWE0" target="_blank"><img class="" src="images/icons/telegram.svg"></img></a>
-          </div>
-          <div class="medium-container">
-            <a target="_blank"><img class="" src="images/icons/medium.svg"></img></a>
-          </div>
-          <center><img class="" src="images/footer.svg"></img></center>
-          <div class="copyright-container">
-            <h5>Copyright © 2022. All rights reserved</h5>
-          </div>
+    <div id="footer">
+      <div class="footer-rectangle">
+        <div class="footer-icon twitter-container">
+          <a href="https://twitter.com/StepFarm" target="_blank"><img class="" src="images/icons/twitter.svg"></img></a>
+        </div>
+        <div class="footer-icon discord-container">
+          <a target="https://twitter.com/StepFarm" href="https://discord.gg/gC72KwH4Gp" target="_blank"><img class="" src="images/icons/discord.svg"></img></a>
+        </div>
+        <div class="footer-icon telegram-container">
+          <a href="https://t.me/+GugtFxYudhtiYWE0" target="_blank"><img class="" src="images/icons/telegram.svg"></img></a>
+        </div>
+        <div class="footer-icon medium-container">
+          <a target="_blank"><img class="" src="images/icons/medium.svg"></img></a>
+        </div>
+        <center><img class="" src="images/footer.svg"></img></center>
+
       </div>
+        <div class="copyright-container">
+          <h5>Copyright © 2022. All rights reserved</h5>
+        </div>
+    </div>
     `
   }
 }
