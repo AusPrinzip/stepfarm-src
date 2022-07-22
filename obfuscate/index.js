259644afe9ba0f640de7b2f85172408a9086c65c
uxlee <lee@stepfarm.io>
2022-07-22 15:57:26 +0200

    Reorganised vendor script files in /lib

--- obfuscate/index.js
+++ obfuscate/index.js
@@ -1,6 +1,6 @@
 var {minify} = require("uglify-js");
-const fs = require("fs")
-
+const fs = require("fs");
+const fse = require('fs-extra');
 var path = require('path');
 // In newer Node.js versions where process is already global this isn't necessary.
 var process = require("process");
@@ -23,3 +23,30 @@ files.forEach(file => {
 // console.log(code)
 const obfuscated = minify(code).code
 fs.writeFileSync(moveTo, obfuscated)
+
+
+// copy everything but "./scripts" to the repo copy
+
+let srcDir = path.resolve(__dirname, "../", "scripts_obfuscated");
+let destDir = path.resolve(__dirname, "../..", "stepfarm_obf", "scripts_obfuscated");
+                              
+// To copy a folder or file  
+fse.copySync(srcDir, destDir, { overwrite: true }, function (err) {
+  if (err) {                
+    console.error(err);       
+  } else {
+    console.log("success!");
+  }
+});
+
+srcDir = path.resolve(__dirname, "../..", "stepfarm");
+destDir = path.resolve(__dirname, "../..", "stepfarm_obf");
+                              
+// To copy a folder or file  
+fse.copySync(srcDir, destDir, { overwrite: false }, function (err) {
+  if (err) {                
+    console.error(err);       
+  } else {
+    console.log("success!");
+  }
+});
